434d590933ce56acd8258e70f652a8ecc531cbbf
ojasvasurawat <surawatojasva@gmail.com>
2024-12-30 20:16:29 +0000

    kuch nahi

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// const { _setMinAndMaxByKey } = require("chart.js/helpers");
+
+
+
+
+
+
+
+
+
 const apiKey = "7038c275b759dfb5e3159826d0c38a3f";
 const url = 'https://api.openweathermap.org/data/2.5/weather?&q=';
 const apiKey1 = "bca34017f9a9fab4408fd5b6e3ed6e05";
@@ -19,7 +29,7 @@ const cityname = searchInput.value;
 // Define an async function to fetch weather data
 async function fetchWeatherData(cityname) {
 	try {
-		const response = await fetch(url + cityname + `&appid=${apiKey}`);
+		const response = await fetch(url + cityname + `&appid=${apiKey}` + `&units=metric`);
 		const result = await response.json();  // Parse response as JSON
 		console.log(result);
 		var lon = result.coord.lon;
@@ -42,7 +52,7 @@ console.log(lon)
 // const apiKey1 = "bca34017f9a9fab4408fd5b6e3ed6e05";
 // const apiUrl = "https://api.openweathermap.org/data/2.5/forecast?";
 async function checkWeather(lat, lon) {
-	const response = await fetch(apiUrl + `lat=` + lat + `&lon=` + lon + `&appid=${apiKey}`);
+	const response = await fetch(apiUrl + `lat=` + lat + `&lon=` + lon + `&appid=${apiKey}` + `&units=metric`);
 
 	if (response.status == 404) {
 		document.querySelector(".error").style.display = "block";
@@ -51,6 +61,46 @@ async function checkWeather(lat, lon) {
 	else {
 		let data = await response.json();
 		console.log(data);
+		var list = data.list;
+		console.log(list);
+		// var map= new Map(list);
+		// console.log(map.get('main.temp'));
+		const temp1 = list.map(obj => obj.main.temp);
+		console.log(temp1);
+		const dt_txt = list.map(obj => obj.dt_txt);
+		console.log(dt_txt);
+		// const date= new Date(dt).toLocaleString;
+		// console.log(date);
+		// let localeDates = dt.map(date => new Date(date).toLocaleString());
+		// console.log(localeDates);
+
+
+
+
+		const ctx = document.getElementById('myChart');
+
+		new Chart(ctx, {
+			type: 'line',
+			data: {
+				labels: [dt_txt],
+				datasets: [{
+					label: '# of Votes',
+					data: [temp1],
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					y: {
+						beginAtZero: false
+					}
+				}
+			}
+		});
+
+
+
+
 	}
 }
 
